36d2f271d8f22f2d305d02a4ba6adc3f321c4b68
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-06-20 18:45:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,4 +8,4 @@ def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction("Dan","Ember")
+introduction_with_languagelearn("Dan","Ember")
